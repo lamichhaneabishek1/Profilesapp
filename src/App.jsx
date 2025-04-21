5db5bcf593be7ee9911837a729519a39300885f7
Lamichhane, Abishek <abisheklamichhane@my.unt.edu>
2025-04-20 19:49:08 -0500

    Added my name to the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,9 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+
       </div>
-      <h1>Vite + React</h1>
+      <h1>Created by Abishek Lamichhane</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
